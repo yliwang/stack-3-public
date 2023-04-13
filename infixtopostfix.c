4d4be26f67405fd3abf80c97c55351af30cf48a9
yliwang <1220264221@qq.com>
2023-04-13 10:28:50 +0800

    Update infixtopostfix.c

--- infixtopostfix.c
+++ infixtopostfix.c
@@ -15,87 +15,8 @@ int precedence(int c)
 	return 0;
 }
 
-void infix_to_postfix(char *infix, char *postfix)
+void infix_to_postfix(char *infix, int *postfix)
 {
-	struct stack s;
-	int len, i, n;
-	int prec;
-
-	init_stack(&s, 50);
-
-	len = strlen(infix);
-
-	n = 0;
-
-	for (i = 0; i < len; i++) {
-		int c = infix[i];
-
-		if (c >= '0' && c <= '9') {
-			postfix[n] = c;
-			n++;
-		}
-		else {
-			switch (c) {
-			case '+':
-			case '-':
-			case '*':
-			case '/':
-				while( !is_empty( &s ) && top(&s) != '(' && precedence(c) <= precedence(top(&s)) ) {
-					/* remove from stack and add to postfix */
-					postfix[n] = pop(&s);
-					n++;
-				}
-
-				/* push c onto stack */
-				push(&s, c);
-				break;
-			case '(':
-				push(&s, '(');
-				break;
-			case ')':
-				while (!is_empty(&s) && top(&s) != '(') {
-					postfix[n] = pop(&s);
-					n++;
-				}
-				/* remove '(' */
-				pop(&s);
-				break;
-			default:
-				assert(0);
-				break;
-			}
-		}
-	}
-
-	/* empty stack */
-	while (!is_empty(&s)) {
-		postfix[n] = pop(&s);
-		n++;
-	}
-
-	postfix[n] = '\0';
-
-	uninit_stack(&s);
+	
 }
 
-void main()
-{
-	char infix[256];
-	char postfix[256];
-
-	//strcpy(infix, "3+4*5");
-
-	//strcpy(infix, "4+5*9");
-	strcpy(infix, "(4+5)*9");
-	//strcpy(infix, "(2+8)*(7/3)");
-	//strcpy(infix, "(3+4)*5");
-	//strcpy(infix, "3*4+5");
-	//strcpy(infix, "((2*5)/(6/4))+(2*3)");
-
-
-	printf("infix:%s\n", infix);
-
-	infix_to_postfix(infix, postfix);
-
-	printf("postfix:%s\n", postfix);
-}
