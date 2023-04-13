627e7df7e4dbcaac8f79106d60e7fc50a9c47d40
yliwang <1220264221@qq.com>
2023-04-13 10:25:32 +0800

    Update stack.h

--- stack.h
+++ stack.h
@@ -10,8 +10,8 @@ struct stack
 	int capacity;
 };
 
-struct stack *init_stack(struct stack *t, int init_cap);
-struct stack *create_stack(int init_cap);
+struct stack *init_stack(struct stack *t);
+struct stack *create_stack();
 
 struct stack *push(struct stack *t, T e);
 
